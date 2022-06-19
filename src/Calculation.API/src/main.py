72c12c90f664c3d92ebb484954635afe04c80972
Richard Dalziel <RichardDalziel@outlook.com>
2022-06-19 13:43:22 +0100

    Switch to Open Telemetry for Fast API

--- src/Calculation.API/src/main.py
+++ src/Calculation.API/src/main.py
@@ -1,45 +1,31 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 import os
 import pandas as pd
-from opencensus.ext.azure.common.protocol import Request
-from opencensus.ext.azure.trace_exporter import AzureExporter
-from opencensus.trace.samplers import ProbabilitySampler
-from opencensus.trace.tracer import Tracer
-from opencensus.trace.span import SpanKind
-from opencensus.trace.attributes_helper import COMMON_ATTRIBUTES
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import (BatchSpanProcessor)
+from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
 
 FilePath = os.getenv('FileShareBasePath')
 AppInsightsConnectionString = os.getenv('APPINSIGHTS_CONNECTION_STRING')
 
-HTTP_URL = COMMON_ATTRIBUTES['HTTP_URL']
-HTTP_STATUS_CODE = COMMON_ATTRIBUTES['HTTP_STATUS_CODE']
-
-APPINSIGHTS_CONNECTION_STRING=AppInsightsConnectionString
-exporter=AzureExporter(connection_string=f'{APPINSIGHTS_CONNECTION_STRING}')
-sampler=ProbabilitySampler(1.0)
+tracer = TracerProvider()
 
 app = FastAPI()
 
-
-@app.middleware("http")
-async def middlewareOpencensus(request: Request, call_next):
-    tracer = Tracer(exporter=exporter, sampler=sampler)
-    with tracer.span("main") as span:
-        span.span_kind = SpanKind.SERVER
-
-        response = await call_next(request)
-
-        tracer.add_attribute_to_current_span(
-            attribute_key=HTTP_STATUS_CODE,
-            attribute_value=response.status_code)
-        tracer.add_attribute_to_current_span(
-            attribute_key=HTTP_URL,
-            attribute_value=str(request.url))
-
-    return response
+@app.on_event("startup")
+def startup_event():
+    exporter = AzureMonitorTraceExporter.from_connection_string(AppInsightsConnectionString)
+    tracer.add_span_processor(BatchSpanProcessor(exporter))
+    FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer)
 
 @app.get("/{filepath}")
-async def load_file(filepath: str):
+async def load_file(filepath: str, response: Response):
     path = os.path.join(FilePath, filepath)
+
+    if not os.path.exists(path):
+        response.status_code = 404
+        return
+
     df = pd.read_csv(path, dtype=str)
     return {"numRows": len(df.index)}
