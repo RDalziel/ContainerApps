3fd01655703a38f13d8662a6d40a4f93ec716be4
Richard Dalziel <RichardDalziel@outlook.com>
2022-06-19 19:26:48 +0100

    Adding Open Telemetry to the .NET API

--- src/Ingestion.API/Program.cs
+++ src/Ingestion.API/Program.cs
@@ -1,7 +1,10 @@
+using Azure.Monitor.OpenTelemetry.Exporter;
 using Ingestion.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
 
 var configBuilder = new ConfigurationBuilder();
 
@@ -11,7 +14,6 @@ configBuilder.AddJsonFile("appsettings.development.json");
 configBuilder.AddJsonFile("appsettings.json");
 configBuilder.AddEnvironmentVariables();
 
-
 var configuration = configBuilder.Build();
 
 var builder = WebApplication.CreateBuilder(args);
@@ -20,6 +22,19 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddOpenTelemetryTracing(builder =>
+{
+    builder.SetResourceBuilder(ResourceBuilder.CreateDefault()
+            .AddService("IngestionAPI")
+            .AddTelemetrySdk()
+            .AddEnvironmentVariableDetector())
+        .AddAspNetCoreInstrumentation()
+        .AddAzureMonitorTraceExporter(o =>
+        {
+            o.ConnectionString = configuration["ApplicationInsightsConnectionString"];
+        });
+});
 
 builder.Services.AddSingleton<IFileDirectory>(new FileDirectory(configuration["FileShareBasePath"]));
 
